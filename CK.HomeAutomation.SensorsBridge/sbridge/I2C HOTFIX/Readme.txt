a5f6fab0912a5f571d2791fca9b74b3d5847b0b9
Christian Kratky <christian.kratky@googlemail.com>
2015-09-28 22:41:26 +0200

    Reorganized the solution.

--- CK.HomeAutomation.SensorsBridge/sbridge/I2C HOTFIX/Readme.txt
+++
@@ -1,4 +0,0 @@
-Fix for support of repeated start conditions in slave mode.
-
-Copy file "twi.c" to "C:\Program Files (x86)\Arduino\hardware\arduino\avr\libraries\Wire\utility"
-Or: Uncomment line 468!
---
+++ SDK/CK.HomeAutomation.Actuators/Automations/AutomaticConditionalOnAutomation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using CK.HomeAutomation.Actuators.Conditions;
+using CK.HomeAutomation.Actuators.Conditions.Specialized;
+using CK.HomeAutomation.Actuators.Contracts;
+using CK.HomeAutomation.Core.Timer;
+
+namespace CK.HomeAutomation.Actuators.Automations
+{
+    public class AutomaticConditionalOnAutomation : Automation
+    {
+        private readonly List<IBinaryStateOutputActuator> _actuators = new List<IBinaryStateOutputActuator>();
+
+        public AutomaticConditionalOnAutomation(IHomeAutomationTimer timer) : base(timer)
+        {
+            WithAutoTrigger(TimeSpan.FromMinutes(1));
+
+            WithActionIfFulfilled(TurnOn);
+            WithActionIfNotFulfilled(TurnOff);
+        }
+
+        public AutomaticConditionalOnAutomation WithOnlyAtNightRange(IWeatherStation weatherStation)
+        {
+            var nightCondition = new TimeRangeCondition(Timer).WithStart(() => weatherStation.Daylight.Sunset).WithEnd(() => weatherStation.Daylight.Sunrise);
+            WithCondition(ConditionRelation.And, nightCondition);
+
+            return this;
+        }
+
+        public AutomaticConditionalOnAutomation WithOffBetweenRange(TimeSpan from, TimeSpan until)
+        {
+            WithCondition(ConditionRelation.AndNot, new TimeRangeCondition(Timer).WithStart(() => from).WithEnd(() => until));
+            return this;
+        }
+
+        public AutomaticConditionalOnAutomation WithActuator(IBinaryStateOutputActuator actuator)
+        {
+            _actuators.Add(actuator);
+            return this;
+        }
+
+        private void TurnOn()
+        {
+            foreach (var actuator in _actuators)
+            {
+                actuator.SetState(BinaryActuatorState.On);
+            }
+        }
+
+        private void TurnOff()
+        {
+            foreach (var actuator in _actuators)
+            {
+                actuator.SetState(BinaryActuatorState.Off);
+            }
+        }
+    }
+}
