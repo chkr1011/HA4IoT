dd04bcd35689860b614eb190d1343be4125834fc
Christian Kratky <christian.kratky@googlemail.com>
2015-10-12 23:45:32 +0200

    Added I2C hardware bridge project.

---
+++ CK.HomeAutomation.Hardware.I2CHardwareBridge/SendLDP433MHzSignalCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Windows.Devices.I2c;
+
+namespace CK.HomeAutomation.Hardware.I2CHardwareBridge
+{
+    public class SendLDP433MhzSignalCommand : I2CHardwareBridgeCommand
+    {
+        private const byte I2C_ACTION_433MHz = 2;
+
+        private uint _code;
+        private byte _length = 24;
+        private byte _pin;
+        private byte _repeats = 2;
+
+        public SendLDP433MhzSignalCommand WithCode(uint code)
+        {
+            _code = code;
+            return this;
+        }
+
+        public SendLDP433MhzSignalCommand WithLength(byte length)
+        {
+            _length = length;
+            return this;
+        }
+
+        public SendLDP433MhzSignalCommand WithPin(byte pin)
+        {
+            _pin = pin;
+            return this;
+        }
+
+        public SendLDP433MhzSignalCommand WithRepeats(byte count)
+        {
+            _repeats = count;
+            return this;
+        }
+
+        public override void Execute(I2cDevice i2CDevice)
+        {
+            i2CDevice.Write(ToPackage());
+        }
+
+        private byte[] ToPackage()
+        {
+            var package = new byte[8];
+            package[0] = I2C_ACTION_433MHz;
+
+            var code = BitConverter.GetBytes(_code);
+            Array.Copy(code, 0, package, 1, 4);
+
+            package[5] = _length;
+            package[6] = _repeats;
+            package[7] = _pin;
+
+            return package;
+        }
+    }
+}
