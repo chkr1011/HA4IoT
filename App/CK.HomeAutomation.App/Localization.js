8d98a9cdbde94bf8c1b4181e529fff45b5bec286
Christian Kratky <christian.kratky@googlemail.com>
2015-10-22 19:02:07 +0200

    App: Localization of UI texts are now supported.

---
+++ App/CK.HomeAutomation.App/Localization.js
@@ -0,0 +1,20 @@
+﻿function getUILocalization(key) {
+    return getLocalizationFromSource(key, uiLocalizations);
+}
+
+function getActuatorLocalization(key) {
+    return getLocalizationFromSource(key, actuatorLocalizations);
+}
+
+function getLocalizationFromSource(key, source) {
+    var result = "#" + key;
+
+    $.each(source, function (i, entry) {
+        if (entry.key === key) {
+            result = entry.value;
+            return;
+        }
+    });
+
+    return result;
+}
