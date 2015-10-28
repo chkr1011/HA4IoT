e133743b06967da9adea0cf78c1904b8be2d9ce1
Christian Kratky <christian.kratky@googlemail.com>
2015-10-28 19:22:33 +0100

    Removed the dependency to the RCSwitch library.

--- SDK/Hardware/HA4IoT.Hardware.I2CHardwareBridge.Firmware/LPD433MhzController.cpp
+++ SDK/Hardware/HA4IoT.Hardware.I2CHardwareBridge.Firmware/LPD433MhzController.cpp
@@ -1,26 +1,48 @@
 #include <Arduino.h>
-#include "RCSwitch.h"
 
 #define DEBUG 0
 
-RCSwitch _rcs = RCSwitch();
+void sendPulses(uint8_t pin, int highCount, int lowCount) {
+	digitalWrite(pin, HIGH);
+	delayMicroseconds(350 * highCount);
+	digitalWrite(pin, LOW);
+	delayMicroseconds(350 * lowCount);
+}
 
-//void LPD433MhzController_setup()
-//{
-//	_rcs.enableReceive(INT1);
-//}
+void send0(uint8_t pin) {
+	sendPulses(pin, 1, 3);
+}
 
-void LPD433MhzController_loop()
+void send1(uint8_t pin) {
+	sendPulses(pin, 3, 1);
+}
+
+void sendSync(uint8_t pin) {
+	sendPulses(pin, 1, 31);
+}
+
+void send(uint8_t data[], uint8_t length, uint8_t repeats, uint8_t pin)
 {
-	//if (!_rcs.available())
-	//{
-	//	return;
-	//}
+	for (uint8_t r = 0; r < repeats; r++)
+	{
+		for (uint8_t i = 0; i < length; i++)
+		{
+			uint8_t buffer = data[i];
+			for (uint8_t bit = 0; bit < 8; bit++)
+			{
+				if (buffer & (1 << bit))
+				{
+					send1(pin);
+				}
+				else
+				{
+					send0(pin);
+				}
+			}
+		}
 
-	//unsigned long receivedCode = _rcs.getReceivedValue();
-	//Serial.println("Received value: " + String(receivedCode) + "@" + String(_rcs.getReceivedBitlength()) + "bits");
-
-	//_rcs.resetAvailable();
+		sendSync(pin);
+	}
 }
 
 void LPD433MhzController_handleI2CWrite(uint8_t package[], uint8_t packageLength)
@@ -41,15 +63,11 @@ void LPD433MhzController_handleI2CWrite(uint8_t package[], uint8_t packageLength
 		return;
 	}
 
-	unsigned long code = (unsigned long)package[3] << 24 | (unsigned long)package[2] << 16 | (unsigned long)package[1] << 8 | (unsigned long)package[0];
+	uint8_t data[] = { package[0], package[1], package[2], package[3] };
 	uint8_t length = package[4];
-	_rcs.setRepeatTransmit(package[5]);
-	_rcs.enableTransmit(package[6]);
+	uint8_t repeats = package[5];
+	uint8_t pin = package[6];
 	
-#if (DEBUG)
-	Serial.println("Sending 433MHz signal with code " + String(code) + " and length " + String(length));
-#endif
-
-	_rcs.send(code, length);
+	pinMode(pin, OUTPUT);
+	send(data, length, repeats, pin);
 }
-
