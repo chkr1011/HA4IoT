4898887f221b3e6b46f1e2367f27956ef2db2d47
Christian Kratky <christian.kratky@googlemail.com>
2015-11-21 17:37:25 +0100

    Add new enums

---
+++ SDK/Hardware/HA4IoT.Hardware.CCTools/HSREL5Output.cs
@@ -0,0 +1,21 @@
+﻿namespace HA4IoT.Hardware.CCTools
+{
+    public enum HSREL5Output
+    {
+        Relay0 = 0,
+
+        Relay1 = 1,
+
+        Relay2 = 2,
+
+        Relay3 = 3,
+
+        Relay4 = 4,
+
+        GPIO0 = 5,
+
+        GPIO1 = 6,
+
+        GPIO2 = 7
+    }
+}
