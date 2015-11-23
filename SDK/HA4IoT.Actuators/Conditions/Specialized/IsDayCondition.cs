458c2e234d70e33f159e7a7d5e0d000d0b1f1f9f
Christian Kratky <christian.kratky@googlemail.com>
2015-11-23 22:51:24 +0100

    Add day and night conditions

---
+++ SDK/HA4IoT.Actuators/Conditions/Specialized/IsDayCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HA4IoT.Contracts;
+using HA4IoT.Contracts.Actuators;
+using HA4IoT.Core.Timer;
+
+namespace HA4IoT.Actuators.Conditions.Specialized
+{
+    public class IsDayCondition : TimeRangeCondition
+    {
+        public IsDayCondition(IWeatherStation weatherStation, IHomeAutomationTimer timer) : base(timer)
+        {
+            if (weatherStation == null) throw new ArgumentNullException(nameof(weatherStation));
+
+            WithStart(() => weatherStation.Daylight.Sunset);
+            WithEnd(() => weatherStation.Daylight.Sunrise);
+        }
+    }
+}
---
+++ SDK/HA4IoT.Actuators/Conditions/Specialized/IsNightCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HA4IoT.Contracts;
+using HA4IoT.Contracts.Actuators;
+using HA4IoT.Core.Timer;
+
+namespace HA4IoT.Actuators.Conditions.Specialized
+{
+    public class IsNightCondition : TimeRangeCondition
+    {
+        public IsNightCondition(IWeatherStation weatherStation, IHomeAutomationTimer timer) : base(timer)
+        {
+            if (weatherStation == null) throw new ArgumentNullException(nameof(weatherStation));
+
+            WithStart(() => weatherStation.Daylight.Sunrise);
+            WithEnd(() => weatherStation.Daylight.Sunset);
+        }
+    }
+}
