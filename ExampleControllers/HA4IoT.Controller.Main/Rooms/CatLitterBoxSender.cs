d57d226fa02c8ece7f82ec7c673a0719728365b4
Christian Kratky <christian.kratky@googlemail.com>
2015-12-12 16:30:27 +0100

    Add counter to cat litter box sender

--- ExampleControllers/HA4IoT.Controller.Main/Rooms/CatLitterBoxSender.cs
+++ ExampleControllers/HA4IoT.Controller.Main/Rooms/CatLitterBoxSender.cs
@@ -13,12 +13,14 @@ namespace HA4IoT.Controller.Main.Rooms
     internal class CatLitterBoxTwitterSender
     {
         private readonly INotificationHandler _log;
-        private const string Suffix = "\r\nTime in litter box: {0}s\r\n@chkratky";
+        private const string Suffix = "\r\nTime in litter box: {0}s\r\nNr. this day: {1}\r\n@chkratky";
 
         private readonly Timeout _timeout = new Timeout(TimeSpan.FromSeconds(30));
         private readonly Random _random = new Random((int)DateTime.Now.Ticks);
         private readonly Stopwatch _timeInLitterBox = new Stopwatch();
 
+        private TimeSpan _effectiveTimeInLitterBox;
+        private int _count = 1;
         private DateTime? _lastTweetTimestamp;
         private string _previousMessage = string.Empty;
 
@@ -84,28 +86,19 @@ namespace HA4IoT.Controller.Main.Rooms
 
         private async Task Tweet(TimeSpan timeInLitterBox)
         {
-            bool tweetingTooFrequently = _lastTweetTimestamp.HasValue && (DateTime.Now - _lastTweetTimestamp) < TimeSpan.FromMinutes(5);
-            if (tweetingTooFrequently)
+            if (GetIsTweetingTooFrequently())
             {
                 return;
             }
 
-            TimeSpan effectiveTimeInLitterBox = timeInLitterBox - _timeout.Duration;
-            bool tooLessTimeInLitterBox = effectiveTimeInLitterBox < TimeSpan.FromSeconds(10);
-            if (tooLessTimeInLitterBox)
+            if (GetDurationIsTooShort(timeInLitterBox))
             {
                 return;
             }
-            
-            string message;
-            do
-            {
-                message = _messages[_random.Next(_messages.Length - 1)];
-            } while (message == _previousMessage);
 
-            _previousMessage = message;
-            message = message + string.Format(Suffix, (int)effectiveTimeInLitterBox.TotalSeconds);
-            
+            UpdateCounter();
+
+            string message = GenerateMessage();
             _log.Verbose("Trying to tweet '" + message + "'.");
 
             try
@@ -125,6 +118,47 @@ namespace HA4IoT.Controller.Main.Rooms
             catch (Exception exception)
             {
                 _log.Warning("Failed to tweet. " + exception.Message);
+            }
+        }
+
+        private string GenerateMessage()
+        {
+            string message;
+            do
+            {
+                message = _messages[_random.Next(_messages.Length - 1)];
+            } while (message == _previousMessage);
+
+            _previousMessage = message;
+
+            return message + string.Format(Suffix, (int)_effectiveTimeInLitterBox.TotalSeconds, _count);
+        }
+
+        private bool GetIsTweetingTooFrequently()
+        {
+            return _lastTweetTimestamp.HasValue && (DateTime.Now - _lastTweetTimestamp) < TimeSpan.FromMinutes(5);
+        }
+
+        private bool GetDurationIsTooShort(TimeSpan timeInLitterBox)
+        {
+            _effectiveTimeInLitterBox = timeInLitterBox - _timeout.Duration;
+            return _effectiveTimeInLitterBox < TimeSpan.FromSeconds(10);
+        }
+
+        private void UpdateCounter()
+        {
+            if (!_lastTweetTimestamp.HasValue)
+            {
+                return;
+            }
+
+            if (_lastTweetTimestamp.Value.Date.Equals(DateTime.Now.Date))
+            {
+                _count++;
+            }
+            else
+            {
+                _count = 1;
             }
         }
 
