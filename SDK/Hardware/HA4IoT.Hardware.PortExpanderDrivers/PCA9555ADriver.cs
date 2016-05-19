0e95c85fac02314af92bae513fae12e72f29f9ed
Christian Kratky <christian.kratky@googlemail.com>
2016-05-19 20:44:11 +0200

    Add address generators for port expanders

--- SDK/Hardware/HA4IoT.Hardware.PortExpanderDrivers/PCA9555ADriver.cs
+++ SDK/Hardware/HA4IoT.Hardware.PortExpanderDrivers/PCA9555ADriver.cs
@@ -4,7 +4,8 @@ namespace HA4IoT.Hardware.PortExpanderDrivers
 {
     public class PCA9555ADriver : PCA9555Driver
     {
-        public PCA9555ADriver(I2CSlaveAddress address, II2CBus i2CBus) : base(address, i2CBus)
+        public PCA9555ADriver(I2CSlaveAddress address, II2CBus i2CBus) 
+            : base(address, i2CBus)
         {
         }
     }
--- SDK/Hardware/HA4IoT.Hardware.PortExpanderDrivers/PCF8574ADriver.cs
+++ SDK/Hardware/HA4IoT.Hardware.PortExpanderDrivers/PCF8574ADriver.cs
@@ -4,7 +4,8 @@ namespace HA4IoT.Hardware.PortExpanderDrivers
 {
     public class PCF8574ADriver : PCF8574Driver
     { 
-        public PCF8574ADriver(I2CSlaveAddress address, II2CBus i2CBus) : base(address, i2CBus)
+        public PCF8574ADriver(I2CSlaveAddress address, II2CBus i2CBus) 
+            : base(address, i2CBus)
         {
         }
     }
