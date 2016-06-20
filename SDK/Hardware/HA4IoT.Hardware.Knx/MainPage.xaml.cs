da9e27ab77046b3e0ad42816ecb04f805d9c9a84
luwidgiani <luwidgiani@hotmail.com>
2016-06-20 20:44:11 +0200

    KNX send receive
    added both async and normal receive of data: i can test both tommorow
    during the day

---
+++ SDK/Hardware/HA4IoT.Hardware.Knx/MainPage.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HA4IoT.Hardware.Knx
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private WebServer webServer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            webServer = new WebServer();
+        }
+
+        private void button_CreateConnection_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ValidateRemoteHost() && ValidateInput())
+                {
+                    // Attempt to connect to the echo server
+                    Log(String.Format("Connecting to server '{0}' over port {1} (echo) ...", textBox_IpAdress.Text, textBox_IpPort.Text), true);
+                    string result = webServer.Connect(textBox_IpAdress.Text, Convert.ToInt32(textBox_IpPort.Text));
+                    Log(result, false);
+                }
+            }
+            catch (Exception ex)
+            {
+                textBlock_Error.Text = @"Connection aborted: 
+                    " + ex.Message;
+            }
+        }
+
+        private void button_SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            Log(String.Format("Sending '{0}' to server ...", textBox_MessageToSend.Text), true);
+            string result = webServer.SendMessage(textBox_MessageToSend.Text);
+            Log(result, false);
+
+            // Receive a response from the echo server
+            Log("Requesting Receive ...", true);
+            result = webServer.Receive();
+            Log(result, false);
+        }
+
+
+        #region UI Validation
+        /// <summary>
+        /// Validates the txtInput TextBox
+        /// </summary>
+        /// <returns>True if the txtInput TextBox contains valid data, otherwise 
+        /// False.
+        ///</returns>
+        private bool ValidateInput()
+        {
+            // txtInput must contain some text
+            if (String.IsNullOrWhiteSpace(textBox_IpPort.Text))
+            {
+                txtOutput.Text += Environment.NewLine + "Please enter the Port to listen to";
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the txtRemoteHost TextBox
+        /// </summary>
+        /// <returns>True if the txtRemoteHost contains valid data,
+        /// otherwise False
+        /// </returns>
+        private bool ValidateRemoteHost()
+        {
+            // The txtRemoteHost must contain some text
+            if (String.IsNullOrWhiteSpace(textBox_IpAdress.Text))
+            {
+                txtOutput.Text += Environment.NewLine + "Please enter a host name";
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region Logging
+        /// <summary>
+        /// Log text to the txtOutput TextBox
+        /// </summary>
+        /// <param name="message">The message to write to the txtOutput TextBox</param>
+        /// <param name="isOutgoing">True if the message is an outgoing (client to server)
+        /// message, False otherwise.
+        /// </param>
+        /// <remarks>We differentiate between a message from the client and server 
+        /// by prepending each line  with ">>" and "<<" respectively.</remarks>
+        private void Log(string message, bool isOutgoing)
+        {
+            string direction = (isOutgoing) ? ">> " : "<< ";
+            txtOutput.Text += Environment.NewLine + direction + message;
+        }
+
+        /// <summary>
+        /// Clears the txtOutput TextBox
+        /// </summary>
+        private void ClearLog()
+        {
+            txtOutput.Text = String.Empty;
+        }
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            taskInstance.GetDeferral();
+            await ThreadPool.RunAsync(workItem =>
+            {
+                webServer.ReceiveAsync();
+            });
+        }
+    }
+}
---
+++ SDK/Hardware/HA4IoT.Hardware.Knx/WebServer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace HA4IoT.Hardware.KNX
+{
+    public class WebServer
+    {
+        Socket _socket = null;
+        static ManualResetEvent _clientDone = new ManualResetEvent(false);
+        const int TIMEOUT_MILLISECONDS = 5000;
+        const int MAX_BUFFER_SIZE = 2048;
+        string port;
+
+        public string Connect(string hostName, int portNumber)
+        {
+            this.port = portNumber.ToString();
+            string result = string.Empty;
+            DnsEndPoint hostEntry = new DnsEndPoint(hostName, portNumber);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+            socketEventArg.RemoteEndPoint = hostEntry;
+            socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate (object s, SocketAsyncEventArgs e)
+            {
+                result = e.SocketError.ToString();
+                _clientDone.Set();
+            });
+
+            _clientDone.Reset();
+            _socket.ConnectAsync(socketEventArg);
+            _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+
+            return result;
+        }
+
+        public string SendMessage(string data)
+        {
+            string response = "Operation Timeout";
+            if (_socket != null)
+            {
+                SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+                socketEventArg.RemoteEndPoint = _socket.RemoteEndPoint;
+                socketEventArg.UserToken = null;
+                socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate (object s, SocketAsyncEventArgs e)
+                {
+                    response = e.SocketError.ToString();
+
+                    _clientDone.Set();
+                });
+
+                byte[] payload = Encoding.UTF8.GetBytes(data);
+                socketEventArg.SetBuffer(payload, 0, payload.Length);
+
+                _clientDone.Reset();
+
+                _socket.SendAsync(socketEventArg);
+
+                _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+            }
+            else
+            {
+                response = "Socket is not initialized";
+            }
+
+            return response;
+        }
+
+        public string Receive()
+        {
+            string response = "Operation Timeout";
+
+            // We are receiving over an established socket connection
+            if (_socket != null)
+            {
+                // Create SocketAsyncEventArgs context object
+                SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+                socketEventArg.RemoteEndPoint = _socket.RemoteEndPoint;
+
+                // Setup the buffer to receive the data
+                socketEventArg.SetBuffer(new Byte[MAX_BUFFER_SIZE], 0, MAX_BUFFER_SIZE);
+
+                // Inline event handler for the Completed event.
+                // Note: This even handler was implemented inline in order to make 
+                // this method self-contained.
+                socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate (object s, SocketAsyncEventArgs e)
+                {
+                    if (e.SocketError == System.Net.Sockets.SocketError.Success)
+                    {
+                        // Retrieve the data from the buffer
+                        response = Encoding.UTF8.GetString(e.Buffer, e.Offset, e.BytesTransferred);
+                        response = response.Trim('\0');
+                    }
+                    else
+                    {
+                        response = e.SocketError.ToString();
+                    }
+
+                    _clientDone.Set();
+                });
+
+                // Sets the state of the event to nonsignaled, causing threads to block
+                _clientDone.Reset();
+
+                // Make an asynchronous Receive request over the socket
+                _socket.ReceiveAsync(socketEventArg);
+
+                // Block the UI thread for a maximum of TIMEOUT_MILLISECONDS milliseconds.
+                // If no response comes back within this time then proceed
+                _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+            }
+            else
+            {
+                response = "Socket is not initialized";
+            }
+
+            return response;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private const uint BufferSize = 8192;
+
+        public async void ReceiveAsync()
+        {
+            StreamSocketListener listener = new StreamSocketListener();
+
+            await listener.BindServiceNameAsync(port);
+
+            listener.ConnectionReceived += async (sender, args) =>
+            {
+                StringBuilder request = new StringBuilder();
+                using (IInputStream input = args.Socket.InputStream)
+                {
+                    byte[] data = new byte[BufferSize];
+                    IBuffer buffer = data.AsBuffer();
+                    uint dataRead = BufferSize;
+                    while (dataRead == BufferSize)
+                    {
+                        await input.ReadAsync(buffer, BufferSize, InputStreamOptions.Partial);
+                        request.Append(Encoding.UTF8.GetString(data, 0, data.Length));
+                        dataRead = buffer.Length;
+                    }
+                }
+
+                using (IOutputStream output = args.Socket.OutputStream)
+                {
+                    using (Stream response = output.AsStreamForWrite())
+                    {
+                        byte[] bodyArray = Encoding.UTF8.GetBytes("<html><body>Hello, World!</body></html>");
+                        var bodyStream = new MemoryStream(bodyArray);
+
+                        var header = "HTTP/1.1 200 OK\r\n" +
+                                    $"Content-Length: {bodyStream.Length}\r\n" +
+                                        "Connection: close\r\n\r\n";
+
+                        byte[] headerArray = Encoding.UTF8.GetBytes(header);
+                        await response.WriteAsync(headerArray, 0, headerArray.Length);
+                        await bodyStream.CopyToAsync(response);
+                        await response.FlushAsync();
+                    }
+                }
+            };
+        }
+    }
+}
