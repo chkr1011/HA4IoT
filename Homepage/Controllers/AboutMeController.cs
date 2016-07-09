b1dd5c1e8747bd50b60a68fa990f788346af4a52
Michael Rohde <ollyvia@gmx.de>
2016-07-09 17:52:36 +0200

    index.cshtml change; impressum, about me, contact side added;

---
+++ Homepage/Controllers/AboutMeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HA4IoT.Homepage.Controllers
+{
+    public class AboutMeController : Controller
+    {
+        // GET: AboutMe
+        public ActionResult AboutMe()
+        {
+            return View();
+        }
+    }
+}
---
+++ Homepage/Controllers/OthersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HA4IoT.Homepage.Controllers
+{
+    public class OthersController : Controller
+    {
+        // GET: Others
+        public ActionResult Impressum()
+        {
+            return View();
+        }
+
+	    public ActionResult Contact()
+	    {
+		    return View();
+	    }
+    }
+}
