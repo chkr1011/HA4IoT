c76c553cc525f5a9f751d230c9cbf8526128b786
Christian Kratky <christian.kratky@googlemail.com>
2016-07-11 19:37:16 +0200

    Update website

--- Homepage/Controllers/AboutMeController.cs
+++ Homepage/Controllers/AboutMeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace HA4IoT.Homepage.Controllers
 {
     public class AboutMeController : Controller
     {
-        // GET: AboutMe
         public ActionResult Index()
         {
             return View();
